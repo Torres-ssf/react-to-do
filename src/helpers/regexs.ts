d5e2eeb460934efe38dfda8fe99d811c11a35421
Torres-ssf <sergio.uft@gmail.com>
2022-07-14 12:19:34 -0300

    add some regexs

---
+++ src/helpers/regexs.ts
@@ -0,0 +1,9 @@
+/**
+ * Should match empty spaces before and after the text
+ */
+export const trimRegex = /^\s+|\s+$/g
+
+/**
+ * Should match any HTML tag insige the text
+ */
+export const filterHtmlRegex = /(<([^>]+)>)/g
